f8d44e4d6e6570391b52631b8b995c64c3614ae7
AK Sarav <sarasoft@live.com>
2019-08-23 18:20:57 +0530

    Removing Firebase Creds

--- src/main.js
+++ src/main.js
@@ -5,18 +5,7 @@ import '@ionic/core/css/ionic.bundle.css';
 import './assets/main.css';
 import Firebase from 'firebase';
 
-let config = {
-    apiKey: "AIzaSyBFUEBYMCRl23SK6YdZXJpLDjG8pn-rINM",
-    authDomain: "saravtodolist.firebaseapp.com",
-    databaseURL: "https://saravtodolist.firebaseio.com",
-    projectId: "saravtodolist",
-    storageBucket: "saravtodolist.appspot.com",
-    messagingSenderId: "1009854098212",
-    appId: "1:1009854098212:web:654c836e3584e9d3"
-}
 
-let app = Firebase.initializeApp(config)
-let db = app.database()
 
 import IonicVue from '@ionic/vue';
 
